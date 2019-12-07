b3a07c5c769d9d3f470e775a8892a0858c622212
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-12-07 09:17:05 -0600

    Updates

---
+++ precache-manifest.06c6f8e83ffbf300cdf7baf67bc63a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63248538115eb7ba54b07874671b343c",
+    "url": "/react-tutorial-beginner/index.html"
+  },
+  {
+    "revision": "218abd60463326c37d69",
+    "url": "/react-tutorial-beginner/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "5514781a505f300825b4",
+    "url": "/react-tutorial-beginner/static/js/2.e58b2eeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial-beginner/static/js/2.e58b2eeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "218abd60463326c37d69",
+    "url": "/react-tutorial-beginner/static/js/main.014e8922.chunk.js"
+  },
+  {
+    "revision": "8f22be8caeddc9b35d4c",
+    "url": "/react-tutorial-beginner/static/js/runtime-main.a49e24f3.js"
+  }
+]);
